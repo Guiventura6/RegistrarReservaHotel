eec3407b88985e4c16fd309989575ac99e677273
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-01 13:19:23 -0300

    Criacao da classe Reservation

---
+++ RegistrarReserva/Entities/Reservation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace RegistrarReserva.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Room {RoomNumber}, check-in: {CheckIn:dd/MM/yyyy}" +
+                $"check-out: {CheckOut:dd//MM/yyyy}, {Duration()} nights");
+            return sb.ToString();
+        }
+    }
+}
